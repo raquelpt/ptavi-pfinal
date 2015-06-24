d6c56ec31e76bdf49ba70f0fea23edb9ab45f390
raquelpt <raquelita_25_4@hotmail.com>
2015-06-24 19:09:04 +0200

    pep8

--- uaserver.py
+++ uaserver.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
-
 """
 Clase (y programa principal) para un servidor en SIP
 """
@@ -21,53 +20,47 @@ P_rtp = ""
 class ServidorHandler(ContentHandler):
 
     def __init__(self):
-
-    """
-    Inicializando el diccionario
-    """
-    
-    self.diccionario = {}
+        """
+        Inicializando el diccionario
+        """
+        self.diccionario = {}
 
     def startElement(self, name, attrs):
-    """
-    Añadiendo atributos al diccionario
-    """
-
-    if name == "account":
-        username = attrs.get("username", "")
-        self.diccionario["userName"] = username
-    if name == "uaserver":
-        ip = attrs.get("ip", "127.0.0.1")
-        puerto = attrs.get("puerto", "")
-        self.diccionario["servIp"] = ip
-        self.diccionario["servPort"] = puerto
-    if name == "rtpaudio":
-        puerto = attrs.get("puerto", "")
-        self.diccionario["rtpPort"] = puerto
-    if name == "regproxy":
-        ip = attrs.get("ip", "")
-        puerto = attrs.get("puerto", "")
-        self.diccionario["proxIp"] = ip
-        self.diccionario["proxPort"] = puerto
-    if name == "log":
-        path = attrs.get("path", "")
-        self.diccionario["logPath"] = path
-    if name == "audio":
-        path = attrs.get("path", "")
-        self.diccionario["audioPath"] = path
+        """
+        Añadiendo atributos al diccionario
+        """
+        if name == "account":
+            username = attrs.get("username", "")
+            self.diccionario["userName"] = username
+        if name == "uaserver":
+            ip = attrs.get("ip", "127.0.0.1")
+            puerto = attrs.get("puerto", "")
+            self.diccionario["servIp"] = ip
+            self.diccionario["servPort"] = puerto
+        if name == "rtpaudio":
+            puerto = attrs.get("puerto", "")
+            self.diccionario["rtpPort"] = puerto
+        if name == "regproxy":
+            ip = attrs.get("ip", "")
+            puerto = attrs.get("puerto", "")
+            self.diccionario["proxIp"] = ip
+            self.diccionario["proxPort"] = puerto
+        if name == "log":
+            path = attrs.get("path", "")
+            self.diccionario["logPath"] = path
+        if name == "audio":
+            path = attrs.get("path", "")
+            self.diccionario["audioPath"] = path
 
     def get_attrs(self):
-    """
-    Lista con los atributos
-    """
-
+        """
+        Lista con los atributos
+        """
         return self.diccionario
-
 
 
 class SipHandler(SocketServer.DatagramRequestHandler):
 
-    
     def handle(self):
 
         while 1:
@@ -81,26 +74,22 @@ class SipHandler(SocketServer.DatagramRequestHandler):
             # Seleccionamos la respuesta correcta
             print "El cliente nos manda " + line
             Metodo = line.split(" ")[0]
-
             if Metodo == "INVITE" or Metodo == "ACK" or Metodo == "BYE":
-
                 if Metodo == "INVITE" and \
-
                     line.split(" ")[2] == 'SIP/2.0\r\n\r\n':
 
                     Answer = "SIP/2.0 100 Trying\r\n\r\n"
                     Answer += "SIP/2.0 180 Ringing\r\n\r\n"
                     Answer += "SIP/2.0 200 OK\r\n\r\n"
                     Answer += "ContentType: application/sdp" + "\r\n\r\n"
-                    Answer += "v=0" + "\r\n"                
+                    Answer += "v=0" + "\r\n"
                     Answer += "o=" + Direccion + " " + P_Server + "\r\n"
                     Answer += "s=misesion" + "\r\n"
                     Answer += "t=0" + "\r\n"
                     Answer += "m=audio " + P_rtp + " RTP" + "\r\n\r\n"
                     self.wfile.write(Answer)
                     print(Answer)
-
-                    accion = "Enviar a " + str(ProxIp) + ":" 
+                    accion = "Enviar a " + str(ProxIp) + ":"
                     accion += str(ProxPort)
                     log(accion, Answer, fich_log)
 
@@ -110,30 +99,25 @@ class SipHandler(SocketServer.DatagramRequestHandler):
                     FICHERO_AUDIO
                     print "Vamos a ejecutar", aEjecutar
                     os.system(aEjecutar)
-                    accion = "Recivido de " + str(ProxyIp) + ":" 
+                    accion = "Recivido de " + str(ProxyIp) + ":"
                     accion += str(Proxy_Port)
                     Answer = FICHERO_AUDIO
                     log(accion, Answer, fich_log)
-
                 elif Metodo == "BYE":
                     # Tratamiento BYE
                     Answer = "SIP/2.0 200 OK\r\n\r\n"
                     self.wfile.write(Answer)
-                    accion = "Sent to " + str(ProxIp) +":"
-                    accion += str(ProxPort) + ":"
-                    log(accion, Answer, fich_log)    
-
-                else:
-
-                    Answer = "SIP/2.0 400 Bad Request"
-                    accion = "Sent to " + str(ProxIp) +":"
+                    accion = "Sent to " + str(ProxIp) + ":"
                     accion += str(ProxPort) + ":"
                     log(accion, Answer, fich_log)
-
+                else:
+                    Answer = "SIP/2.0 400 Bad Request"
+                    accion = "Sent to " + str(ProxIp) + ":"
+                    accion += str(ProxPort) + ":"
+                    log(accion, Answer, fich_log)
             else:
-
                 Answer = "SIP/2.0 405 Method Not Allowed"
-                accion = "Sent to " + str(ProxIp) +":"
+                accion = "Sent to " + str(ProxIp) + ":"
                 accion += str(ProxPort) + ":"
                 log(accion, Answer, fich_log)
 
@@ -143,36 +127,32 @@ class SipHandler(SocketServer.DatagramRequestHandler):
                 print "Enviamos:" + Answer
                 self.wfile.write(Answer)
 
-
     def log(self, accion, linea, logFile):
-    """
-    Metodo para poder imprimir debug por pantalla y log en el fichero
-    """
-    
-        tiempo = time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))  
+        """
+        Metodo para poder imprimir debug por pantalla y log en el fichero
+        """
+
+        tiempo = time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))
         line = linea.split("\r\n", " ")
         texto = ' '.join(line)
         logFile = open(LOG_FILE, 'a')
         logFile.write(tiempo + ' ' + line + '\n')
         logFile.close()
 
-
 if __name__ == "__main__":
 
-
     if len(sys.argv) != 2:
-
         print "Usage: python uaserver.py config"
         raise SystemExit
 
     Config = sys.argv[1]
-    
+
     parser = make_parser()
     Datos = SipHandler()
     parser.setContentHandler(Datos)
     parser.parse(open(Config))
-    Dicc = SipHandler_Datos.get_attrs()
 
+    Dicc = SipHandler_Datos.get_attrs()
     USERNAME = Dicc['userName']
     IP = Dicc['servIp']
     PORT = int(Dicc['servPort'])
