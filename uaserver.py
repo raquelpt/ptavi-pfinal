e09df5f27f55dec12c25e4ecb3211979f57129ed
raquelpt <raquelita_25_4@hotmail.com>
2015-06-24 11:35:26 +0200

    Update uaserver.py

--- uaserver.py
+++ uaserver.py
@@ -62,7 +62,100 @@ class ServidorHandler(ContentHandler):
     """
 
         return self.diccionario
-        
+
+
+
+class SipHandler(SocketServer.DatagramRequestHandler):
+
+    
+    def handle(self):
+
+        while 1:
+            # Leyendo línea a línea lo que nos envía el cliente
+            line = self.rfile.read()
+
+            # Si no hay más líneas salimos del bucle infinito
+            if not line:
+                break
+
+            # Seleccionamos la respuesta correcta
+            print "El cliente nos manda " + line
+            Metodo = line.split(" ")[0]
+
+            if Metodo == "INVITE" or Metodo == "ACK" or Metodo == "BYE":
+
+                if Metodo == "INVITE" and \
+
+                    line.split(" ")[2] == 'SIP/2.0\r\n\r\n':
+
+                    Answer = "SIP/2.0 100 Trying\r\n\r\n"
+                    Answer += "SIP/2.0 180 Ringing\r\n\r\n"
+                    Answer += "SIP/2.0 200 OK\r\n\r\n"
+                    Answer += "ContentType: application/sdp" + "\r\n\r\n"
+                    Answer += "v=0" + "\r\n"                
+                    Answer += "o=" + Direccion + " " + P_Server + "\r\n"
+                    Answer += "s=misesion" + "\r\n"
+                    Answer += "t=0" + "\r\n"
+                    Answer += "m=audio " + P_rtp + " RTP" + "\r\n\r\n"
+                    self.wfile.write(Answer)
+                    print(Answer)
+
+                    accion = "Enviar a " + str(ProxIp) + ":" 
+                    accion += str(ProxPort)
+                    log(accion, Answer, fich_log)
+
+                elif Metodo == "ACK":
+                    # Tratamiento ACK
+                    aEjecutar = "mp32rtp -i 127.0.0.1 -p 23032 < " + \
+                    FICHERO_AUDIO
+                    print "Vamos a ejecutar", aEjecutar
+                    os.system(aEjecutar)
+                    accion = "Recivido de " + str(ProxyIp) + ":" 
+                    accion += str(Proxy_Port)
+                    Answer = FICHERO_AUDIO
+                    log(accion, Answer, fich_log)
+
+                elif Metodo == "BYE":
+                    # Tratamiento BYE
+                    Answer = "SIP/2.0 200 OK\r\n\r\n"
+                    self.wfile.write(Answer)
+                    accion = "Sent to " + str(ProxIp) +":"
+                    accion += str(ProxPort) + ":"
+                    log(accion, Answer, fich_log)    
+
+                else:
+
+                    Answer = "SIP/2.0 400 Bad Request"
+                    accion = "Sent to " + str(ProxIp) +":"
+                    accion += str(ProxPort) + ":"
+                    log(accion, Answer, fich_log)
+
+            else:
+
+                Answer = "SIP/2.0 405 Method Not Allowed"
+                accion = "Sent to " + str(ProxIp) +":"
+                accion += str(ProxPort) + ":"
+                log(accion, Answer, fich_log)
+
+            # Imprimimos la respuesta y la enviamos
+
+            if Metodo != "ACK":
+                print "Enviamos:" + Answer
+                self.wfile.write(Answer)
+
+
+    def log(self, accion, linea, logFile):
+    """
+    Metodo para poder imprimir debug por pantalla y log en el fichero
+    """
+    
+        tiempo = time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))  
+        line = linea.split("\r\n", " ")
+        texto = ' '.join(line)
+        logFile = open(LOG_FILE, 'a')
+        logFile.write(tiempo + ' ' + line + '\n')
+        logFile.close()
+
 
 if __name__ == "__main__":
 
