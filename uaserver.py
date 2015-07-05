4e1cde34c8ee4c3d86f16102e4f9e1a3a813ca25
raquelpt <raquelita_25_4@hotmail.com>
2015-07-05 12:06:18 +0200

    Update uaserver.py

--- uaserver.py
+++ uaserver.py
@@ -11,13 +11,10 @@ import os.path
 import socket
 
 
-
-
 class SipHandler(SocketServer.DatagramRequestHandler):
 
-
     def handle(self):
-		
+
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
@@ -27,12 +24,12 @@ class SipHandler(SocketServer.DatagramRequestHandler):
             # Seleccionamos la respuesta correcta
             print "El cliente nos manda " + line
             Metodo = line.split(" ")[0]
-			# Comprobamos el metodo que nos han mandado
+            # Comprobamos el metodo que nos han mandado
             if Metodo == "INVITE" or Metodo == "ACK" or Metodo == "BYE":
-				
+
                 if Metodo == "INVITE":
-					# Si el metodo es invite enviamos la correspondiente 
-					# respuesta
+                    # Si el metodo es invite enviamos la correspondiente
+                    # respuesta
                     Answer = 'SIP/2.0 100 Trying' + '\r\n\r\n'
                     Answer += "SIP/2.0 180 Ringing\r\n\r\n"
                     Answer += "SIP/2.0 200 OK\r\n\r\n"
@@ -41,13 +38,15 @@ class SipHandler(SocketServer.DatagramRequestHandler):
                     Answer += "o=" + USUARIO + " " + IP + "\r\n"
                     Answer += "s=misesion" + "\r\n"
                     Answer += "t=0" + "\r\n"
-                    Answer += "m=audio " + str(PUERTO_AUDIO) + " RTP" + "\r\n\r\n"
+                    Answer += "m=audio " + str(PUERTO_AUDIO) + \
+                              " RTP" + "\r\n\r\n"
                     self.wfile.write(Answer)
                     print(Answer)
 
                 elif Metodo == "ACK":
                     # Tratamiento ACK
-                    aEjecutar = "mp32rtp -i 127.0.0.1 -p" + str(PUERTO_AUDIO) + " < " + PATH_AUDIO
+                    aEjecutar = "mp32rtp -i 127.0.0.1 -p" + \
+                                str(PUERTO_AUDIO) + " < " + PATH_AUDIO
                     print "Vamos a ejecutar", aEjecutar
                     os.system(aEjecutar)
                 elif Metodo == "BYE":
@@ -56,67 +55,60 @@ class SipHandler(SocketServer.DatagramRequestHandler):
                     self.wfile.write(Answer)
                     print (Answer)
             else:
-				# No se trata de ninguno de los metodos permitidos
+                # No se trata de ninguno de los metodos permitidos
                 Answer = "SIP/2.0 405 Method Not Allowed"
                 print Answer
                 self.wfile.write(Answer)
 
 
-          
-
 if __name__ == "__main__":
 
+    try:
+        if len(sys.argv) != 2:
+            print "Usage: python uaserver.py config"
+            raise SystemExit
 
-	try:
+        Config = sys.argv[1]
 
-		if len(sys.argv) != 2:
-		    print "Usage: python uaserver.py config"
-		    raise SystemExit
+        fich = open(Config, 'r')
+        line = fich.readlines()
+        fich.close()
 
-		Config = sys.argv[1]
+        #CLIENTE
+        lineusuario = line[1].split(">")
+        cuenta = lineusuario[0].split("=")[1]
+        USUARIO = cuenta.split(" ")[0][1:-2]
+        #IP
+        lineserver = line[2].split(">")
+        uaserver = lineserver[0].split("=")[1]
+        IP = uaserver.split(" ")[0][1:-1]
+        #Port
+        puertserver = lineserver[0].split("=")[2]
+        PORT = puertserver.split(" ")[0][1:-2]
+        #IP DEL PROXY
+        lineipproxy = line[4].split(">")
+        ipproxy = lineipproxy[0].split("=")[1]
+        IP_PROXY = ipproxy.split(" ")[0][1:-1]
+        #PUERTO DEL PROXY
+        puertoproxy = lineipproxy[0].split("=")[2]
+        PORT_PROXY = puertoproxy.split(" ")[0][1:-2]
+        #Port AUDIO RTP
+        lineaudiortp = line[3].split(">")
+        rtpaudio = lineaudiortp[0].split("=")[1]
+        PUERTO_AUDIO = rtpaudio.split(" ")[0][1:-2]
+        #PATH DEL AUDIO
+        linedeaudio = line[6].split(">")
+        pathaudio = linedeaudio[0].split("=")[1]
+        PATH_AUDIO = pathaudio.split(" ")[0][1:-2]
 
-		fich = open(Config, 'r')
-		line = fich.readlines()
-		fich.close()
+        my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((IP_PROXY, int(PORT_PROXY)))
+        print "Server: " + USUARIO + " listening at port " + \
+        str(PORT) + "..." + "\r\n"
+        print "Listening..."
 
-
-		#CLIENTE
-		lineusuario = line[1].split(">")
-		cuenta = lineusuario[0].split("=")[1]
-		USUARIO = cuenta.split(" ")[0][1:-2]
-		#IP
-		lineserver = line[2].split(">")
-		uaserver = lineserver[0].split("=")[1]
-		IP = uaserver.split(" ")[0][1:-1]
-		#Port
-		puertserver = lineserver[0].split("=")[2]
-		PORT = puertserver.split(" ")[0][1:-2]
-		#IP DEL PROXY
-		lineipproxy = line[4].split(">")
-		ipproxy = lineipproxy[0].split("=")[1]
-		IP_PROXY = ipproxy.split(" ")[0][1:-1]
-		#PUERTO DEL PROXY
-		puertoproxy = lineipproxy[0].split("=")[2]
-		PORT_PROXY = puertoproxy.split(" ")[0][1:-2]
-		#Port AUDIO RTP
-		lineaudiortp = line[3].split(">")
-		rtpaudio = lineaudiortp[0].split("=")[1]
-		PUERTO_AUDIO = rtpaudio.split(" ")[0][1:-2]
-		#PATH DEL AUDIO
-		linedeaudio = line[6].split(">")
-		pathaudio = linedeaudio[0].split("=")[1]
-		PATH_AUDIO = pathaudio.split(" ")[0][1:-2]
-
-	
-
-		my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		my_socket.connect((IP_PROXY, int(PORT_PROXY)))
-		print "Server: " + USUARIO + " listening at port " + \
-		str(PORT) + "..." + "\r\n"
-		print "Listening..."
-		
-		serv = SocketServer.UDPServer(("127.0.0.1", int(PORT)), SipHandler)
-		serv.serve_forever()
-	except IndexError:
-		sys.exit("Usage: python uaserver.py config")
+        serv = SocketServer.UDPServer(("127.0.0.1", int(PORT)), SipHandler)
+        serv.serve_forever()
+    except IndexError:
+        sys.exit("Usage: python uaserver.py config")
